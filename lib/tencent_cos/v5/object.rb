e033f2af6c2165da88883fa8b571f841db59a6ab
atpking <atpking@gmail.com>
2017-11-13 15:37:24 +0800

    add more functions

--- lib/tencent_cos/v5/object.rb
+++ lib/tencent_cos/v5/object.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'digest'
 
 module TencentCos
@@ -6,20 +8,37 @@ module TencentCos
       def upload_token(file_name, custom_headers = {})
         auth(file_name, "put", {}, custom_headers)
       end
-      
+
+      def key_exists?(dict)
+        fetch_meta(dict)
+        true
+      rescue RestClient::NotFound => _e
+        false
+      end
+
+      def delete_object(dict = {})
+        raise "need key at least" if dict[:key].nil?
+        uri = "#{config.host(dict[:bucket], dict[:region])}/#{dict[:key]}"
+        do_request(uri, "delete", {}, {}, auth: true)
+      end
+
+      def fetch_meta(dict = {})
+        raise "need key at least" if dict[:key].nil?
+        uri = "#{config.host(dict[:bucket], dict[:region])}/#{dict[:key]}"
+        do_request(uri, "head", {}, {}, auth: true)
+      end
+
       # options 存在以下字段
       # expired_key 过期的expired_key 如果有这个值  就认为是私有的  否则直接返回
-      # 
+      #
       def download_url(http_base, file_path, options = {})
         unless options[:expired_key].nil?
           uri_info = timespan_download_url_auth(file_path, options[:expired_key], options[:expired_second])
           return "#{http_base}#{file_path}?#{uri_info[:answer]}"
         end
         "#{http_base}#{file_path}"
-
-
       end
-     
+
       private
 
       def timespan_download_url_auth(file_path, expired_key, expired_second = nil)
@@ -27,7 +46,7 @@ module TencentCos
         expired_second ||= 30 * 60
         # sign = MD5(KEY+ path + t)
         # http://www.test.com/ folder /vodfile.mp4?sign=abc123dsaadsasdads&t=4d024e80
-        #sign=abc123dsaadsasdads&t=4d024e80
+        # sign=abc123dsaadsasdads&t=4d024e80
         time = Time.now.to_i + expired_second
         time_hex = time.to_s(16)
         sign = Digest::MD5.hexdigest "#{expired_key}#{file_path}#{time_hex}"
@@ -37,7 +56,6 @@ module TencentCos
           answer: "sign=#{sign}&t=#{time_hex}"
         }
       end
-
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -15,13 +15,38 @@ end
 module TencentCos
   class ClientSpec
     RSpec.describe Client do
+      it 'could fetch meta' do
+        client = Client.new
+        VCR.use_cassette("fetch_meta") do
+          response = client.fetch_meta(key: "4c33e8e91cde2680dd0cc549541d0be785a9beac.apk")
+          expect(response.code).to eq 200
+
+          expect do
+            response = client.fetch_meta(key: "not_found")
+          end.to raise_error(RestClient::NotFound)
+        end
+        VCR.use_cassette("file_exists") do
+          expect(client.key_exists?(key: "4c33e8e91cde2680dd0cc549541d0be785a9beac.apk")).to eq true
+          expect(client.key_exists?(key: "not_found")).to eq false
+        end
+      end
+      
+      it 'could delete file object' do
+        client = Client.new
+        VCR.use_cassette("delete_object") do
+          response = client.delete_object(key: "4c33e8e91cde2680dd0cc549541d0be785a9beac.apk")
+          expect(response.code).to eq 204
+        end
+      end
+
+
       it 'could list bucket correctly' do
         client = Client.new
         VCR.use_cassette("bucket_list") do
           xml = client.bucket_list
         end
       end
-      
+
       it 'could build access_token for upload' do
         client = Client.new do |client|
           client.auth_helper.force_sign_time = "1417773892;1417853898" # 固定时间戳
@@ -30,10 +55,10 @@ module TencentCos
         tmp = client.upload_token("helloworld")
         expect(tmp).to eq "q-sign-algorithm=sha1&q-ak=AKIDoYOIMWduEQgnnERYclnxBcQ3ZJ58OKeO&q-sign-time=1417773892;1417853898&q-key-time=1417773892;1417853898&q-header-list=&q-url-param-list=&q-signature=9eccc028b9414c4752d70c26aeb67d006ec77ef3"
       end
-      
+
       it 'could build timspan download url' do
-        client = Client.new 
-        Timecop.freeze(Time.at(1509993031 - 30 * 60 )) do 
+        client = Client.new
+        Timecop.freeze(Time.at(1_509_993_031 - 30 * 60)) do
           url = client.download_url("http://pro-app-tc.fir.im/", '123.apk', expired_key: "tencent_given_key")
           expect(url).to eq "http://pro-app-tc.fir.im/123.apk?sign=ce0b276d23d18bf9ca0baa51b0d9bbaf&t=5a00aa47"
         end
